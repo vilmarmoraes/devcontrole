a7d30eaea3932c87b56a7143ae9432dd69bcf4c6
vilmarmoraes <vilmarpomifrai@hotmail.com>
2024-04-11 17:21:51 -0300

    dashboard criado

---
+++ src/components/container/index.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export function Container({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <div className="w-full max-w-7xl mx-auto px-2">
+        {children}
+      </div>
+    </>
+  );
+}
