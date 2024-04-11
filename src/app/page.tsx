1eeb319839d8627a75a3ca7ed647dd96d0fe18a0
vilmarmoraes <vilmarpomifrai@hotmail.com>
2024-04-11 16:21:57 -0300

    header e home criado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-//import Image from "next/image";
+import Image from "next/image";
+import heroImg from "../assets/hero.svg";
 
 export default function Home() {
   return (
-    <main>
-      <h1>TESTE</h1>
+    <main className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)]">
+      <h2 className="font-medium text-2xl mb-2">Gerencie sua empresa</h2>
+      <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">
+        Atendimento, clientes
+      </h1>
+      <Image
+        src={heroImg}
+        alt="Imagem do hero do dev controle"
+        width={600}
+        className="max-w-sm md:max-w-xl"
+      />
     </main>
   );
 }
